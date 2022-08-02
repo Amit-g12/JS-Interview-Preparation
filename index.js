da0abba8196e6f801248472a7eb2165325bc9ca8
Amit Kumar <amitkumargang123@gmail.com>
2022-08-02 13:38:56 +0530

    day2

---
+++ index.js
@@ -0,0 +1,39 @@
+// var a = 10;
+// function add()
+// {
+//     var a = 10;
+//     console.log(a);
+// }
+// add()
+// console.log(a);
+
+// let a =10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+// a = 10; //initialization
+// console.log(a);
+// var a;// declaration
+
+// var a;
+// console.log(a);
+// a = 10;
+
+// function add() {
+//     console.log(a);
+//     console.log(b);
+
+//     var a =10;
+//     let b = 20;
+// }
+
+var a = 10;
+
+var b = "10";
+
+console.log(a===b);
+
+
+
+//we can access var variable within the scope to the outside scope.
